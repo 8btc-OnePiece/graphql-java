b937e7a9d547f70ac375f0c60782dc9209666d24
wanghaifengnihaoa <1440290144@qq.com>
2023-01-30 11:22:32 +0800

    feat:GraphQLLong 兼容IntValue convert&GraphQLBigDecimal 兼容FloatValue、IntValue convert

--- src/main/java/graphql/Scalars.java
+++ src/main/java/graphql/Scalars.java
@@ -623,7 +623,7 @@ public class Scalars {
             } else if (input instanceof FloatValue) {
                 return ((FloatValue) input).getValue();
             } else if (input instanceof IntValue) {
-                return new BigDecimal((IntValue) input.getValue().toString());
+                return new BigDecimal(((IntValue) input).getValue().toString());
             }
             return null;
 
